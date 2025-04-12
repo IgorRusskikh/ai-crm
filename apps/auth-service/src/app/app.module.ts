6914383854a43978c32c78875315473a37f5b62e
IgorRusskikh <russkikh200@mail.ru>
2025-04-12 15:01:07 +0700

    feat: set global setting for guard and config

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -1,8 +1,24 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { APP_GUARD } from '@nestjs/core';
+import { ControllersModule } from '../infrastructure/controllers/controllers.module';
+import { JwtAuthGuard } from '../shared/guards/jwt-auth.guard';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ControllersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    ConfigService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
