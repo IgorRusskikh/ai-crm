fb4e3d76e408253e5a2d6490e1793c2cb0d22c44
IgorRusskikh <russkikh200@mail.ru>
2025-04-12 14:59:10 +0700

    feat: add service to control refresh token db

---
+++ apps/auth-service/src/infrastructure/services/tokens/tokens.service.ts
@@ -0,0 +1,97 @@
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
+
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+import { JwtPayload } from 'apps/auth-service/src/app/interfaces/jwt-payload';
+import { PrismaPersistence } from '../../persistence/prisma.persistence';
+
+@Injectable()
+export class TokensService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+    private readonly prisma: PrismaPersistence
+  ) {}
+
+  async createOrUpdateRefreshToken(tokenDto: {
+    userId: string;
+    token: string;
+    expiresAt: Date;
+  }) {
+    const { userId, ...rest } = tokenDto;
+
+    return this.prisma.refreshToken.upsert({
+      where: {
+        token: rest.token,
+      },
+      update: {
+        ...rest,
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+      create: {
+        ...rest,
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  }
+
+  async deleteRefreshTokensByUserId(userId: string) {
+    return this.prisma.refreshToken.deleteMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  async deleteRefreshTokenByToken(token: string) {
+    return this.prisma.refreshToken.delete({
+      where: {
+        token,
+      },
+    });
+  }
+
+  async signAccessToken(payload: JwtPayload) {
+    return this.signJwt({
+      payload,
+      options: {
+        expiresIn: this.configService.get('ACCESS_TOKEN_EXPIRATION_TIME'),
+      },
+    });
+  }
+
+  async signRefreshToken(payload: JwtPayload) {
+    return this.signJwt({
+      payload,
+      options: {
+        expiresIn: this.configService.get('REFRESH_TOKEN_EXPIRATION_TIME'),
+      },
+    });
+  }
+
+  async verifyAccessToken(token: string) {
+    return this.jwtService.verify(token);
+  }
+
+  async verifyRefreshToken(token: string) {
+    return this.jwtService.verify(token);
+  }
+
+  private async signJwt<T extends object | Buffer>({
+    payload,
+    options,
+  }: {
+    payload: T;
+    options: JwtSignOptions;
+  }) {
+    return this.jwtService.sign(payload, options);
+  }
+}
