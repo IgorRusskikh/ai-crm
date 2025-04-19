45afcbb3e575c75ab65a673755157e1b90f995af
IgorRusskikh <russkikh200@mail.ru>
2025-04-19 12:37:03 +0700

    feat: move redis client in sinle file

--- apps/api-gateway/src/app/shared/common/rate-limiter.ts
+++ apps/api-gateway/src/app/shared/common/rate-limiter.ts
@@ -1,13 +1,5 @@
 import { RateLimiterRedis } from 'rate-limiter-flexible';
-import Redis from 'ioredis';
-
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASSWORD,
-  db: Number(process.env.REDIS_DB),
-  enableOfflineQueue: false,
-});
+import { redisClient } from './redis';
 
 export const rateLimiter = new RateLimiterRedis({
   storeClient: redisClient,
