60f5aa380c32af5c28761c7cbfc4b81910f7488f
IgorRusskikh <russkikh200@mail.ru>
2025-04-19 12:52:48 +0700

    feat: add new service to manage users and stores records

--- apps/user-store-service/src/app/app.module.ts
+++ apps/user-store-service/src/app/app.module.ts
@@ -1,12 +1,25 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { PrismaPersistence } from '../infrastructure/persistence/prisma.persistence';
+import { StoresService } from '../infrastructure/services/stores/stores.service';
+import { StoresUseCase } from './use-cases/stores/stores.use-case';
+import { UsersController } from '../infrastructure/controllers/users/users.controller';
+import { UsersService } from '../infrastructure/services/users/users.service';
+import { UsersUseCase } from './use-cases/users/users.use-case';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+  ],
+  controllers: [UsersController],
+  providers: [
+    PrismaPersistence,
+    UsersService,
+    UsersUseCase,
+    StoresService,
+    StoresUseCase,
   ],
 })
 export class AppModule {}
