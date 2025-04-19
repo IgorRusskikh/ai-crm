ccf62ad5491e62c1fe52a514536e7897126b3982
IgorRusskikh <russkikh200@mail.ru>
2025-04-19 12:38:58 +0700

    feat: move massive code in services

--- apps/api-gateway/src/app/features/authentication/auth.controller.ts
+++ apps/api-gateway/src/app/features/authentication/auth.controller.ts
@@ -1,36 +1,32 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Inject,
-  InternalServerErrorException,
   Ip,
-  Logger,
   Post,
   Req,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { Request } from 'express';
-import { catchError, firstValueFrom, timeout } from 'rxjs';
+import { Request, Response } from 'express';
+import { Public } from '../../shared/decorators/public.decorator';
 import { LocalAuthGuard } from '../../shared/guards/local-auth.guard';
 import { SignUpDto } from './auth.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  private readonly logger = new Logger(AuthController.name);
-
-  constructor(
-    private readonly authService: AuthService,
-    @Inject('AUTH_SERVICE') private readonly client: ClientProxy
-  ) {
-    this.client.connect();
-  }
+  constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
   @UseGuards(LocalAuthGuard)
-  async signin(@Req() req: Request, @Ip() ip: string) {
+  @Public()
+  async signin(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+    @Ip() ip: string
+  ) {
     const signInDto = req.body as unknown as {
       email: string;
     };
@@ -41,53 +37,25 @@ export class AuthController {
       await this.authService.resetLoginRateLimit(signInDto.email, ip);
     }
 
-    const user = req.user;
-
-    return user;
+    return this.authService.handleSignIn(
+      req.user,
+      ip,
+      req.headers['user-agent'] || 'unknown',
+      res
+    );
   }
 
   @Post('signup')
+  @Public()
   async signup(
     @Req() req: Request,
     @Ip() ip: string,
     @Body() signUpDto: SignUpDto
   ) {
-    const userCreateDto = {
-      user: {
-        email: signUpDto.email,
-        password: signUpDto.password,
-        phoneNumber: signUpDto.phoneNumber,
-      },
-      ip: ip,
-      userAgent: req.headers['user-agent'] || 'unknown',
-    };
-
-    try {
-      const response = await firstValueFrom(
-        this.client.send('auth.signup', userCreateDto).pipe(
-          timeout(15000),
-          catchError((err) => {
-            this.logger.error(
-              `Ошибка при регистрации: ${err.message}`,
-              err.stack
-            );
-            throw err;
-          })
-        )
-      );
-
-      this.logger.log(`Получен ответ: ${JSON.stringify(response)}`);
-      return response;
-    } catch (error) {
-      this.logger.error(`Ошибка при регистрации: ${error.message}`);
-
-      if (error.message === 'Timeout has occurred') {
-        throw new InternalServerErrorException(
-          'Превышено время ожидания ответа от сервиса аутентификации'
-        );
-      }
-
-      throw new InternalServerErrorException(error.message);
-    }
+    return this.authService.handleSignUp(
+      signUpDto,
+      ip,
+      req.headers['user-agent'] || 'unknown'
+    );
   }
 }
--- apps/api-gateway/src/app/features/authentication/auth.service.ts
+++ apps/api-gateway/src/app/features/authentication/auth.service.ts
@@ -1,13 +1,30 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 import { rateLimiter } from '../../shared/common/rate-limiter';
 import { v4 as uuidv4 } from 'uuid';
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, firstValueFrom, catchError, timeout } from 'rxjs';
+import { SignUpDto } from './auth.dto';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
-  constructor(@Inject('AUTH_SERVICE') private readonly client: ClientProxy) {}
+  private readonly logger = new Logger(AuthService.name);
+
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly client: ClientProxy,
+    @Inject('USER_STORE_SERVICE') private readonly userStoreClient: ClientProxy
+  ) {
+    this.client.connect();
+    this.userStoreClient.connect();
+  }
 
   async checkLoginRateLimit(email: string, ip: string) {
     const key = `login_fail:${email}:${ip}`;
@@ -31,5 +48,73 @@ export class AuthService {
     const key = `login_fail:${email}:${ip}`;
 
     await rateLimiter.delete(key);
+  }
+
+  async handleSignIn(user: any, ip: string, userAgent: string, res: Response) {
+    try {
+      const data = await lastValueFrom(
+        this.client.send('auth.signin', {
+          user,
+          ip,
+          userAgent,
+        })
+      );
+
+      if (!data.accessToken || !data.cookieOptions) {
+        throw new UnauthorizedException('Неверные учетные данные');
+      }
+
+      res.cookie('access_token', data.accessToken, data.cookieOptions);
+      return data;
+    } catch (error) {
+      this.logger.error(`Ошибка при входе: ${error.message}`, error.stack);
+      throw error;
+    }
+  }
+
+  async handleSignUp(signUpDto: SignUpDto, ip: string, userAgent: string) {
+    const userCreateDto = {
+      user: {
+        email: signUpDto.email,
+        password: signUpDto.password,
+        phoneNumber: signUpDto.phoneNumber,
+      },
+      ip: ip,
+      userAgent: userAgent,
+    };
+
+    try {
+      const response = await firstValueFrom(
+        this.client.send('auth.signup', userCreateDto).pipe(
+          timeout(15000),
+          catchError((err) => {
+            this.logger.error(
+              `Ошибка при регистрации: ${err.message}`,
+              err.stack
+            );
+            throw err;
+          })
+        )
+      );
+
+      const userStoreResponse = await lastValueFrom(
+        this.userStoreClient.send('users.create', {
+          authId: response.userId,
+          phoneNumber: signUpDto.phoneNumber,
+        })
+      );
+
+      return response;
+    } catch (error) {
+      this.logger.error(`Ошибка при регистрации: ${error.message}`);
+
+      if (error.message === 'Timeout has occurred') {
+        throw new InternalServerErrorException(
+          'Превышено время ожидания ответа от сервиса аутентификации'
+        );
+      }
+
+      throw new InternalServerErrorException(error.message);
+    }
   }
 }
