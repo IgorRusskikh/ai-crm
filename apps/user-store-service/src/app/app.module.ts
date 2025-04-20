0d3d9ce144318c014bfad31d1486cb41525d7442
IgorRusskikh <russkikh200@mail.ru>
2025-04-20 17:49:33 +0700

    feat: add new deps in user-store-service module

--- apps/user-store-service/src/app/app.module.ts
+++ apps/user-store-service/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PrismaPersistence } from '../infrastructure/persistence/prisma.persistence';
+import { StoresController } from '../infrastructure/controllers/stores/stores.controller';
 import { StoresService } from '../infrastructure/services/stores/stores.service';
 import { StoresUseCase } from './use-cases/stores/stores.use-case';
+import { UserStoreController } from '../infrastructure/controllers/user-store/user-store.controller';
+import { UserStoreService } from '../infrastructure/services/user-store/user-store.service';
+import { UserStoreUseCase } from './use-cases/user-store/user-store.use-case';
 import { UsersController } from '../infrastructure/controllers/users/users.controller';
 import { UsersService } from '../infrastructure/services/users/users.service';
 import { UsersUseCase } from './use-cases/users/users.use-case';
@@ -13,13 +17,15 @@ import { UsersUseCase } from './use-cases/users/users.use-case';
       isGlobal: true,
     }),
   ],
-  controllers: [UsersController],
+  controllers: [UsersController, StoresController, UserStoreController],
   providers: [
     PrismaPersistence,
     UsersService,
     UsersUseCase,
     StoresService,
     StoresUseCase,
+    UserStoreService,
+    UserStoreUseCase,
   ],
 })
 export class AppModule {}
